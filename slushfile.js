2e3270c388f9ba9ded28badffcde0027d4811e67
Victor Tang <victortang1992@gmail.com>
2016-04-25 18:27:11 -0700

    bower install when all done

--- slushfile.js
+++ slushfile.js
@@ -103,7 +103,7 @@ gulp.task('default', function (done) {
                 }))
                 .pipe(conflict('./'))
                 .pipe(gulp.dest('./'))
-                //.pipe(install())
+                .pipe(install())
                 .on('end', function () {
                     done();
                 });
